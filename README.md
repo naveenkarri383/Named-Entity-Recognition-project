1b7f993f16eafcff37752ab30d5b364499c00ff6
naveenkarri383 <karrinaveen.mme@gmail.com>
2024-08-16 14:11:32 +0530

    Requirements added

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Named-Entity-Recognition-project
+
+## Git commands
+
+```bash
+git add .
+
+git commit -m "Updated"
+
+git push origin main
+```
--- template.py
+++ template.py
@@ -5,7 +5,7 @@ import logging
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 
-project_name = "ner"
+project_name = "ner_project"
 
 list_of_files = [
     f"{project_name}/components/__init__.py",
