ad40e8604ca547a5d16e6d59338616ec1d32446f
naveenkarri383 <karrinaveen.mme@gmail.com>
2024-08-19 15:33:34 +0530

    Data Ingestion and data transformation added

--- train.py
+++ train.py
@@ -1,3 +1,27 @@
-from ner.configuration.gcloud import GCloud
-obj=GCloud()
-obj.sync_folder_from_gcloud(gcp_bucket_url="ner-using-bert-383",filename="archive.zip", destination="test")
+# Confirm weather data retrieval will be takes place or not.
+#from ner.configuration.gcloud import GCloud
+#obj=GCloud()
+#obj.sync_folder_from_gcloud(gcp_bucket_url="ner-using-bert-383",filename="archive.zip", destination="test1")
+
+#Delete test file 
+
+import os
+import sys
+from ner.exception import NerException
+from ner.pipeline.train_pipeline import TrainPipeline
+
+from ner.constants import *
+
+
+def training():
+    try:
+        train_pipeline = TrainPipeline()
+
+        train_pipeline.run_pipeline()
+
+    except Exception as e:
+        raise NerException(e, sys) from e
+
+
+if __name__ == "__main__":
+    training()
